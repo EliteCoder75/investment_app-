a0d4597d41b12f28f25fe1f3fbaa0fa272bc0a47
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2025-03-24 08:58:49 +0100

    change the logic of sending the data, we use an obj that contains the user's inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports:[ HeaderComponent, UserInputComponent]
+  imports:[ HeaderComponent, UserInputComponent, InvestmentResultsComponent]
 })
-export class AppComponent {}
+export class AppComponent {
+  
+
+  onCalculateInvestment (data: {
+    initial_inv : number;
+    annual_inv : number;
+    expected_ret : number;
+    duration : number;
+  }) {
+    const annualData = [];
+    let investmentValue = data.initial_inv;
+  
+    for (let i = 0; i < data.duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (data.expected_ret / 100);
+      investmentValue += interestEarnedInYear + data.annual_inv;
+      const totalInterest =
+      investmentValue - data.annual_inv * year - data.initial_inv;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: data.annual_inv,
+        totalInterest: totalInterest,
+        totalAmountInvested: data.initial_inv + data.annual_inv * year,
+      });
+    }
+  
+    //this.emitData.emit(annualData);
+    console.log(annualData)  }
+}
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,31 +9,28 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  initial_inv: number = 0;
-  annual_inv: number = 0;
-  expected_ret: number = 0;
-  duration :number = 0;
 
-  calculate () {
-    const annualData = [];
-    let investmentValue = this.initial_inv;
-  
-    for (let i = 0; i < this.duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (this.expected_ret / 100);
-      investmentValue += interestEarnedInYear + this.annual_inv;
-      const totalInterest =
-        investmentValue - this.annual_inv * year - this.initial_inv;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: this.annual_inv,
-        totalInterest: totalInterest,
-        totalAmountInvested: this.initial_inv + this.annual_inv * year,
-      });
-    }
-  
-    console.log (annualData);  }
+  @Output() emitData: EventEmitter<{
+    initial_inv : number;
+    annual_inv : number;
+    expected_ret : number;
+    duration : number;
+  }> = new EventEmitter();
+
+  //default values
+  initial_inv = '0';
+  annual_inv = '0';
+  expected_ret = '5';
+  duration = '10';
+
+
+  onSubmit () {
+    this.emitData.emit({
+      initial_inv : +this.initial_inv,
+      annual_inv : +this.annual_inv,
+      expected_ret : +this.expected_ret,
+      duration : +this.duration
+    });
+  }
 
 }
